892452757146e1cc92302fc94b1391cb0f51e218
ToanLa <lqtoan20@gmail.com>
2023-07-25 20:38:40 +0700

    style: add comment for bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# Create variable to store city data
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }

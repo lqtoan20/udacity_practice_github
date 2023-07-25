be76296c96c58ce244384f3cd9e8b1e71b7048f9
ToanLa <lqtoan20@gmail.com>
2023-07-25 20:41:10 +0700

    refactor: add print statement when end program

--- bikeshare.py
+++ bikeshare.py
@@ -203,6 +203,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('Stop program !')
             break
 
 
